f19729cd70b6c143331f09b3a6b84b8e0f017bac
speaking-hedge <hecke@naberius.de>
2014-04-29 22:23:38 +0200

    stop all containers before delete
    ...and ease the image rm...

--- test/clear_containers.sh
+++ test/clear_containers.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "*** this will whipe out all local containers and images ***"
+echo "*** this will stop and whipe out all local containers and images ***"
 
 read -p "proceed (N/y)?" asw
 
+sudo docker.io stop $(sudo docker.io ps -a -q --no-trunc)
+
 if [ "X$asw" = "Xy" -o "X$asw" = "XY" ] ; then
-	if [ $(sudo docker.io ps -a --no-trunc -q | wc -l) -gt 0 ] ; then
+	if [ $(sudo docker.io ps -a -q | wc -l) -gt 0 ] ; then
 		sudo docker.io rm $(sudo docker.io ps -a --no-trunc -q)
-		sudo docker.io rmi $(sudo docker.io images | tail -n+2 | awk '{ print $3}')
+		sudo docker.io rmi $(sudo docker.io images -a --no-trunc -q)
 	fi
 fi
